c9c49d7eb51894809cdf9294c70372b6116c76df
miaoyu <miaoyu2009@qq.com>
2023-03-18 23:50:53 +0800

    feat: add vant

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
+import "vant/lib/index.css";
+import { Button } from "vant";
 </script>
 
 <template>
-  <div>hello world</div>
+  <div :class="$style.box">
+    <Button :type="'primary'">Select</Button>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style module>
+.box {
 }
 </style>
