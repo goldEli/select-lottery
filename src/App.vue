3af0a1f0aa9ac4df9506a5035645fe6de539c327
miaoyu <miaoyu2009@qq.com>
2023-03-19 17:47:04 +0800

    feat: remove

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import "vant/lib/index.css";
-import { Button } from "vant";
+import { Button, showConfirmDialog } from "vant";
 import { useLotteryList } from "./api/lottery";
 import { computed, watch, watchEffect } from "vue";
 import { useLocalStorage } from "@vueuse/core";
@@ -14,7 +14,6 @@ const lastedNum = computed(() => {
   return parseInt(list.value?.[0].lotteryDrawNum ?? "") + 1;
 });
 const store = useLocalStorage<string[]>("lottery-data", []);
-const myLottery = ["10", "12", "24", "30", "33", "9", "12"];
 
 watchEffect(() => {
   console.log(store.value);
@@ -22,6 +21,20 @@ watchEffect(() => {
 
 const onAdd = () => {
   store.value.push(lastedNum.value + "");
+};
+const onRemove = (drawNum: string) => {
+  showConfirmDialog({
+    title: "Title",
+    message: "Delete it?",
+  })
+    .then(() => {
+      // on confirm
+      const idx = store.value.findIndex((item) => item === drawNum);
+      store.value.splice(idx, 1);
+    })
+    .catch(() => {
+      // on cancel
+    });
 };
 const current = (drawNum: string) => {
   return list.value?.find((item) => item.lotteryDrawNum == drawNum);
@@ -32,7 +45,12 @@ const current = (drawNum: string) => {
   <div :class="$style.box">
     <Button :type="'primary'" @click="onAdd">Add</Button>
     <ul :class="$style.list">
-      <li :class="$style.listItem" v-for="item in store" :key="item">
+      <li
+        @click="onRemove(current(item)?.lotteryDrawNum ?? '')"
+        :class="$style.listItem"
+        v-for="item in store"
+        :key="item"
+      >
         <div :class="$style.titleBox">
           <span>{{ item ?? current(item)?.lotteryDrawNum }}</span>
           <span>{{ current(item)?.lotteryDrawTime }}</span>
